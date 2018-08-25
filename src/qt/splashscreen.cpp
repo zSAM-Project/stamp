e5d605b5c548206380359669c19aba605e7f0d5a
zSAM-Project <40679306+zSAM-Project@users.noreply.github.com>
2018-08-25 18:26:24 +0700

    Update splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -27,9 +27,9 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
 {
     // set reference point, paddings
     int paddingLeft = 14;
-    int paddingTop = 470;
+    int paddingTop = 458;
     int titleVersionVSpace = 17;
-    int titleCopyrightVSpace = 44;
+    int titleCopyrightVSpace = 32;
 
     float fontFactor = 1.0;
 
